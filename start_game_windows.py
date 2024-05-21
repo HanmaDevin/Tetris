1389d26ae4f6b766e36b40aa45bfe95286c3a8c9
HanmaDevin <devin12@icloud.com>
2024-05-21 21:33:08 +0200

    new try

--- start_game_windows.py
+++ start_game_windows.py
@@ -1,7 +1,13 @@
 #! /bin/python
+import subprocess
 import os
+
+def run(cmd):
+    completed = subprocess.Popen('powershell.exe [cmd]')
+    return completed
 
 print("Welcome to the Tetris-Game starter script")
 print("First we are going to install pygame")
 
-os.system('powershell.exe [python3 --version]')
+cmd = "python3 --version" 
+run(cmd)
