d2076fa3a04ea5037229ab8a9a887cd8a11b6b9f
HanmaDevin <devinbc12@icloud.com>
2024-05-21 19:12:05 +0200

    new import

--- block.py
+++ block.py
@@ -1,4 +1,4 @@
-from color import Color
+from colors import Colors
 import pygame as pg
 from position import Position
 
@@ -6,33 +6,38 @@ class Block:
     def __init__(self, id):
         self.id = id
         self.cells = {}
-        self.cellSize = 30
-        self.rowOffset = 0
-        self.colOffset = 0
-        self.rotation = 0
-        self.color = Color.getCellColor()
+        self.cell_size = 30
+        self.row_offset = 0
+        self.col_offset = 0
+        self.rotation_state = 0
+        self.colors = Colors.get_cell_colors()
 
     def move(self, rows, cols):
-        self.rowOffset += rows
-        self.colOffset += cols
+        self.row_offset += rows
+        self.col_offset += cols
 
     def getCellPosition(self):
-        tiles = self.cells[self.rotation]
-        movedTile = []
+        tiles = self.cells[self.rotation_state]
+        movedTiles = []
         for position in tiles:
-            position = Position(position.row + self.rowOffset, position.col + self.colOffset)
-            movedTile.append(position)
-        return movedTile
+            position = Position(position.row + self.row_offset, position.col + self.col_offset)
+            movedTiles.append(position)
+        return movedTiles
     
     def rotate(self):
-        self.rotation += 1
-        if self.rotation == len(self.cells):
-            self.rotation = 0
+        self.rotation_state += 1
+        if self.rotation_state == len(self.cells):
+            self.rotation_state = 0
+
+    def undoRotation(self):
+        self.rotation_state -= 1
+        if self.rotation_state == 0:
+            self.rotation_state = len(self.cells) - 1
 
 
     def draw(self, screen):
         tiles = self.getCellPosition()
         for tile in tiles:
-            tileRect = pg.Rect(tile.col * self.cellSize + 1, tile.row * self.cellSize + 1,
-                               self.cellSize - 1, self.cellSize - 1)
-            pg.draw.rect(screen, self.color[self.id], tileRect)
+            tileRect = pg.Rect(tile.col * self.cell_size + 1, tile.row * self.cell_size + 1,
+                               self.cell_size - 1, self.cell_size - 1)
+            pg.draw.rect(screen, self.colors[self.id], tileRect)
