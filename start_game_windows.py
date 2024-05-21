66246ad4084e4f43a62a6140d40def6a2e6cb25f
HanmaDevin <devin12@icloud.com>
2024-05-22 00:40:37 +0200

    added script

--- start_game_windows.py
+++ start_game_windows.py
@@ -1,13 +1,40 @@
 #! /bin/python
-import subprocess
-import os
 
-def run(cmd):
-    completed = subprocess.Popen('powershell.exe [cmd]')
-    return completed
+import os, time
 
-print("Welcome to the Tetris-Game starter script")
-print("First we are going to install pygame")
+def exec(cmd):
+    return os.system(cmd)
 
-cmd = "python3 --version" 
-run(cmd)
+print("Welcome to the Tetris-Game starter script\n")
+print("First we are going to install pygame\n")
+
+if exec("pip --version") != 0:
+    exec("winget install pip")
+
+print("Now we are are going to install pygame\n")
+
+exec("pip install pygame")
+
+print("Now we need to install git, if not already installed\n")
+
+if exec("git --version") != 0:
+    exec("winget install git")
+
+print("You will have to configure git, but i am going to help you\n")
+email = input("You need to type your github email adress: ")
+user_name = input("Type your github username: ")
+
+exec("git config --global user.name " + user_name)
+exec("git config --global user.email " + email)
+
+print("Almost done, cloning the repository\n")
+repo = "https://github.com/HanmaDevin/Tetris"
+exec("git clone " + repo)
+
+print("Only thing to do is to change the directory and start the game\n")
+exec("cd Tetris/game")
+
+print("Have fun with the game")
+time.sleep(1)
+
+exec("python3 main.py")
